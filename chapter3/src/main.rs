3aa5d6efd184d0dcbfdd641db6158da40da97c53
Anne-Marie <Anne-Marie.Kourieh@student.uclouvain.be>
2024-03-24 00:23:15 +0100

    3 - Tuple Structs

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,4 @@
+/* Health Statistics
 struct User {
     name: String,
     age: u32,
@@ -52,3 +53,174 @@ fn test_set_age() {
     bob.set_age(33);
     assert_eq!(bob.age(), 33);
 }
+*/
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Point {
+    x : i64,
+    y : i64
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Point { x, y }
+    }
+
+    fn magnitude(&self) -> f64 {
+        ((self.x.pow(2) + self.y.pow(2)) as f64).sqrt()
+    }
+
+    fn dist(&self, other: Point) -> f64 {
+        (((self.x - other.x).pow(2) + (self.y - other.y).pow(2)) as f64).sqrt()
+    }
+
+}
+
+impl std::ops::Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, _rhs: Point) -> Point {
+        Point {x: self.x + _rhs.x,
+            y: self.y + _rhs.y }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Polygon {
+    points : Vec<Point>
+}
+
+impl Polygon {
+    fn new() -> Self {
+        Polygon {
+            points: Vec::new()
+        }
+    }
+
+    fn add_point(&mut self, point: Point) {
+        self.points.push(point);
+    }
+
+    fn left_most_point(&self) -> Option<Point> {
+        self.points.iter().min_by_key(|point| point.x).cloned()
+    }
+
+    fn perimeter(&self) -> f64 {
+        let mut perimeter = 0.0;
+        let mut prev_point = self.points.last().unwrap();
+        for point in self.points.iter() {
+            perimeter += prev_point.dist(*point);
+            prev_point = point;
+        }
+        perimeter
+    }
+}
+
+pub struct Circle {
+    center : Point,
+    radius : i32
+}
+
+impl Circle {
+    fn new(center: Point, radius: i32) -> Self {
+        Circle {
+            center,
+            radius
+        }
+    }
+
+    fn perimeter(&self) -> f64 {
+        2.0 * std::f64::consts::PI * self.radius as f64
+    }
+}
+
+#[derive()]
+pub enum Shape {
+    Polygon(Polygon),
+    Circle(Circle),
+}
+
+impl Shape {
+    fn perimeter(&self) -> f64 {
+        match self {
+            Shape::Polygon(poly) => poly.perimeter(),
+            Shape::Circle(circle) => circle.perimeter()
+        }
+
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn round_two_digits(x: f64) -> f64 {
+        (x * 100.0).round() / 100.0
+    }
+
+    #[test]
+    fn test_point_magnitude() {
+        let p1 = Point::new(12, 13);
+        assert_eq!(round_two_digits(p1.magnitude()), 17.69);
+    }
+
+    #[test]
+    fn test_point_dist() {
+        let p1 = Point::new(10, 10);
+        let p2 = Point::new(14, 13);
+        assert_eq!(round_two_digits(p1.dist(p2)), 5.00);
+    }
+
+    #[test]
+    fn test_point_add() {
+        let p1 = Point::new(16, 16);
+        let p2 = p1 + Point::new(-4, 3);
+        assert_eq!(p2, Point::new(12, 19));
+    }
+
+    #[test]
+    fn test_polygon_left_most_point() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+        assert_eq!(poly.left_most_point(), Some(p1));
+    }
+
+    #[test]
+    fn test_polygon_iter() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+
+        let points = poly.points.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(points, vec![Point::new(12, 13), Point::new(16, 16)]);
+    }
+
+    #[test]
+    fn test_shape_perimeters() {
+        let mut poly = Polygon::new();
+        poly.add_point(Point::new(12, 13));
+        poly.add_point(Point::new(17, 11));
+        poly.add_point(Point::new(16, 16));
+        let shapes = vec![
+            Shape::from(Shape::Polygon(poly)),
+            Shape::from(Shape::Circle(Circle::new(Point::new(10, 20), 5))),
+        ];
+        let perimeters = shapes
+            .iter()
+            .map(Shape::perimeter)
+            .map(round_two_digits)
+            .collect::<Vec<_>>();
+        assert_eq!(perimeters, vec![15.48, 31.42]);
+    }
+}
+
+#[allow(dead_code)]
+fn main() {}
